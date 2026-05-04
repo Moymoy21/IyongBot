2c5e3cf59c876ac14c61a2f18f8181ac223382b2
Moymoy21 <45497441+Moymoy21@users.noreply.github.com>
2026-05-05 03:02:42 +0800

    Create create_boot.js

---
+++ src/interactions/buttons/create_boot.js
@@ -0,0 +1,22 @@
+// src/interactions/buttons/create_boot.js
+
+module.exports = {
+    customId: 'create_boot', // Ito dapat ang ID ng button mo
+    async execute(interaction) {
+        // Ang listahan ng pet images mo
+        const petList = [
+            "https://static.wikia.nocookie.net/growagarden/images/3/3c/Dilophosaurus.png/revision/latest?cb=20250712071322",
+            "https://link-ng-image-2.png",
+            "https://link-ng-image-3.png"
+        ];
+
+        // Format: "1 link", "2 link", etc.
+        const formattedList = petList.map((link, index) => `${index + 1} ${link}`).join('\n');
+
+        await interaction.reply({
+            content: `**🐾 Pet List:**\n${formattedList}`,
+            ephemeral: false // Gawing true kung gusto mo ay private reply
+        });
+    },
+};
+
