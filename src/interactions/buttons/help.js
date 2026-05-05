c736ebded926467eec5433699d3a7e779ae23b98
Moymoy21 <45497441+Moymoy21@users.noreply.github.com>
2026-05-05 12:15:01 +0800

    Update help.js

--- src/interactions/buttons/help.js
+++ src/interactions/buttons/help.js
@@ -1,21 +1,8 @@
-import { SlashCommandBuilder } from "discord.js";
-import { InteractionHelper } from '../../utils/interactionHelper.js';
-import { createEmptyHelp, createActiveListingPage, activeListings } from '../../handlers/helpSelectMenus.js';
+import { helpCategorySelectMenu, petNext, petPrev, petRemove } from '../../handlers/helpSelectMenus.js';
 
-export default {
-    data: new SlashCommandBuilder()
-        .setName("help")
-        .setDescription("View your active pet listings"),
-
-    async execute(interaction, guildConfig, client) {
-        try {
-            await InteractionHelper.safeDefer(interaction);
-            const response = (activeListings && activeListings.length > 0) 
-                ? createActiveListingPage(0) 
-                : createEmptyHelp();
-            await InteractionHelper.safeEditReply(interaction, response);
-        } catch (error) {
-            console.error(error);
-        }
-    },
-};
+export default [
+    helpCategorySelectMenu,
+    petNext,
+    petPrev,
+    petRemove
+];
